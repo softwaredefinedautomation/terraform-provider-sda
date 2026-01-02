78c2c4afc230faa19a70fa7038cc265245ca47eb
Amir Javanshir <amir.javanshir@softwaredefinedautomation.io>
2026-01-02 18:22:00 +0100

    adjusted the provider name to softwaredefinedautomation/sda instead of the initial sda/sda

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		Address: "registry.terraform.io/sda/sda",
+		Address: "registry.terraform.io/softwaredefinedautomation/sda",
 		Debug:   debug,
 	}
 
